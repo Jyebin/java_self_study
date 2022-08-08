1b6625c29c6c7f90a04507b658940345aa17d131
Jyebin <yebin0322@naver.com>
2022-08-08 23:38:50 +0900

    2022.08.08

---
+++ coding/java/eclipse self study/chap01/src/sec02/exam05/StringExample.java
@@ -0,0 +1,19 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "홍길동";
+		String job = "프로그래머";
+		String str1 = "나는 \"자바\"를 좋아합니다.";
+		String str2 = "번호\t이름\t나이";
+		String str3 = "홍길동\n감자바";
+		System.out.println(name);
+		System.out.println(job);
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+
+	}
+
+}
