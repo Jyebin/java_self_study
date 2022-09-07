e3ba910668b065bc27b9ac4674165d25fffb04de
Jyebin <yebin0322@naver.com>
2022-09-08 00:32:56 +0900

    2022.09.08

---
+++ coding/java/eclipse self study/OpenChallenge1/src/MyInfoApp.java
@@ -0,0 +1,11 @@
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+		System.out.println("학번 : 202011095");
+		System.out.println("이름 : 조예빈");
+		System.out.println("전공 학과 : 컴퓨터과학과");
+
+	}
+
+}
