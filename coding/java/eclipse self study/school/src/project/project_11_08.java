9543c86b6663b0c35f28f6d7eb15771f8eb9e82a
Jyebin <yebin0322@naver.com>
2022-11-12 02:46:31 +0900

    2022.11.12

--- coding/java/eclipse self study/school/src/project/project_11_08.java
+++ coding/java/eclipse self study/school/src/project/project_11_08.java
@@ -5,34 +5,24 @@ import java.util.Scanner;
 
 public class project_11_08 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Vector<String> namev = new Vector<String>();
-		Vector <Integer> pricev = new Vector<Integer>();
-		Vector <Integer> nv = new Vector<Integer>();
-		Vector <Integer> soldv = new Vector<Integer>();
-
-		for(int i=0; i<namev.size(); i++) {
-			String name = null;
-			int price = 0;
-			int n = 0;
-			int sold = 0;
-			name = sc.next();
-			price = sc.nextInt();
-			n = sc.nextInt();
-			sold = sc.nextInt();
-			namev.add(name);
-			pricev.add(price);
-			nv.add(n);
-			soldv.add(sold);
-		}
-		for(int i=0;i<namev.size();i++) {
-			System.out.print(namev.get(i) + " ");
-			System.out.print(pricev.get(i)+ " ");
-			System.out.print(nv.get(i) + " ");
-			System.out.print(soldv.get(i));
-		}
-	}
+		Vector<Goods> goodsVector = new Vector<Goods>(3); //Goods Vector 선언 -> 초기 용량은 3으로
+        Scanner s = new Scanner(System.in); //스캐너
+        for (int i = 0; i < goodsVector.capacity(); i++) { //goodsvector의 용량 만큼 반복문
+            String name = s.next();
+            int price = s.nextInt();
+            int n = s.nextInt();
+            int sold = s.nextInt();
+            goodsVector.add(new Goods(name, price, n, sold)); //벡터에 새로운 굿즈 클래스를 선언하며 값 넣어주기
+        }
+        for (int i = 0; i < goodsVector.size(); i++) { //벡터에 값 들어가있는 만큼 반복문
+            System.out.print(goodsVector.get(i).getName() + " "); //출력
+            System.out.print(goodsVector.get(i).getPrice() + " ");
+            System.out.print(goodsVector.get(i).getNumberOfStock() + " ");
+            System.out.println(goodsVector.get(i).getSold());
+        }
+    }
 }
+
 class Goods{
 	private String name;
 	private int numberOfStock;
