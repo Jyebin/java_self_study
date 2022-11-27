1e2c8887d780cb7c08a3671c47d31c6e31a27b47
Jyebin <yebin0322@naver.com>
2022-11-27 23:57:32 +0900

    2022.11.27

---
+++ git/self_study/p38.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+/*
+ 작성자 : 조예빈
+ 작성일 : 2022.08.06
+ */
+
+public class p38 { //프로그램 실행 진입점
+	
+	public static void main(String[] args) { //콘솔에 출력하는 실행문 
+		System.out.println("Hello, Java");
+
+	} //end of main
+
+} //end of class
---
+++ git/self_study/혼공씨/p388.c
@@ -0,0 +1,27 @@
+//
+//  p388.c
+//  study
+//
+//  Created by 조예빈 on 2022/08/04.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num[3][4] = { //2차원 배열의 선언과 초기화
+        {1, 2, 3, 4}, //num의 0행
+        {5, 6, 7, 8}, //num의 1행
+        {9, 10, 11, 12} //num의 2행
+    };
+    
+    int i,j;
+    
+    for(i=0;i<3;i++){
+        for(j=0;j<4;j++){
+            printf("%5d",num[i][j]); //배열 요소 출력
+        }
+        printf("\n"); //한 행을 출력한 후에 줄 바꿈
+    }
+    return 0;
+}
