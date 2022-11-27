50bad5affff1133d281df14057c138e62e588e22
Jyebin <yebin0322@naver.com>
2022-11-27 18:24:30 +0900

    사진없어서오류뜸

---
+++ coding/ios/MyOwnStyleApp/MyOwnStyleApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyOwnStyleApp
+//
+//  Created by 조예빈 on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+    @IBAction func modelbutton(_ sender: Any) {
+        
+    }
+}
+
